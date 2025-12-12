0ed04cf2c0b3b0ff3f4093ca31dd021757963f3d
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-12 17:37:08 +0100

    3.01 "done"
    follow up later for necessary improvements

---
+++ Array3.01.js
@@ -0,0 +1,6 @@
+month = new Array("January","February","March","April","May","June","July","August","September","Octuber","November","December");
+
+
+
+alert(month);
+/**mejorar, hacer una funcion que imprima los string del array y que el alert llame a esa funcion*/
