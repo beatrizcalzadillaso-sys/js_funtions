6ec9102ad22681e7a6d2f3f29f722a0e62b9ed38
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-12 17:51:37 +0100

    3.02 "done"
    improvements for showing the names

---
+++ Array3.02.js
@@ -0,0 +1,7 @@
+nombre= new Array(3);
+
+nombre[0]=prompt("Introduzca el primer nombre");
+nombre[1]=prompt("Introduzca el segundo nombre");
+nombre[2]=prompt("Introduzca el tercer nombre");
+
+alert("Los nombre son "+ nombre);
